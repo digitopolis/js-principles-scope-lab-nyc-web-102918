6a3e2cde87a15832eefc408bb6adc23c9331b46d
Matt Readout <matt.readout@gmail.com>
2018-12-09 22:39:35 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'robert';
+
+function upperCaseCustomerName(name) {
+  customerName = customerName.toUpperCase();
+};
+
+function setBestCustomer(name) {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(name) {
+  bestCustomer = name;
+}
+
+function changeLeastFavoriteCustomer(name) {
+  leastFavoriteCustomer = name;
+}
